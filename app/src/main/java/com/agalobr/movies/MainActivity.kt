339135c620b0dbb3734d49d5fb941ec214f81db2
alvarogo1998 <rovala18@hotmail.com>
2022-11-10 16:50:48 +0100

    Add folders for movies data_remote_retrofit

--- app/src/main/java/com/agalobr/movies/MainActivity.kt
+++ app/src/main/java/com/agalobr/movies/MainActivity.kt
@@ -2,12 +2,9 @@ package com.agalobr.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.agalobr.movies.data.remote.retrofit.ApiClient
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
