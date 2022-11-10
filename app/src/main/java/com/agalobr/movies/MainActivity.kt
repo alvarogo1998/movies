9c8ad79759ddfefda7255fcc59c55df89926d629
alvarogo1998 <rovala18@hotmail.com>
2022-11-10 16:50:18 +0100

    Add folders for movies data_remote_retrofit

--- app/src/main/java/com/agalobr/movies/MainActivity.kt
+++ app/src/main/java/com/agalobr/movies/MainActivity.kt
@@ -7,20 +7,11 @@ import com.agalobr.movies.data.remote.retrofit.ApiClient
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-
-    val apiClient = ApiClient()
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Thread {
-            val movies = apiClient.getAllMovies()
-            Log.d("@dev", "Movies: $movies")
-            val movie = apiClient.getMovieById("intestellar")
-            Log.d("@dev", "Movie: $movie")
-        }.start()
     }
-
 
 }
