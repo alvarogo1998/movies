6a2e5ad6adf9ba70b820ff51ddd18619623dda03
alvarogo1998 <rovala18@hotmail.com>
2022-12-01 00:12:49 +0100

    Add MovieFragment, RecyclerView and ViewModel

--- app/src/main/java/com/agalobr/movies/MainActivity.kt
+++ app/src/main/java/com/agalobr/movies/MainActivity.kt
@@ -2,13 +2,21 @@ package com.agalobr.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.agalobr.movies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setupBinding()
     }
 
+    private fun setupBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.let {
+            setContentView(it.root)
+        }
+    }
 }
